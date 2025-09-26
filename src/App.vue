80a8db8701e20911780f8c4792f6977ca2ff4273
Suchanad12 <67026494@up.ac.th>
2025-09-26 21:44:13 +0700

    PopupProfile

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script setup>
-  import MainNavbar from './components/MainNavbar.vue'
+import MainNavbar from './components/MainNavbar.vue'
+import { ref } from "vue";
+import UserPopup from "./components/Popupprofile.vue";
+
+const showPopup = ref(false);
+
+const user = {
+  firstName: "สมพล",
+  lastName: "หยดย้อย",
+  studentId: "67022928",
+  phone: "0617584567"
+};
 </script>
 
 <template>
-  <div class="page">
-    <MainNavbar />
-    <h1>
-      Hello World, hello nong pee eng tee
-    </h1>
+  <div>
+    <button @click="showPopup = true">ดูโปรไฟล์</button>
+
+    <UserPopup 
+      :show="showPopup" 
+      :user="user" 
+      @close="showPopup = false"
+    />
   </div>
 </template>
-
