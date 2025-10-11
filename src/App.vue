a60bf327484c918c4a5a62325c452e92883a2f1a
Altroria_Arts <gamesaaa001@gmail.com>
2025-10-11 21:02:05 +0700

    เพิ่มตารางจอง(ยังไม่สมบูรณ์)

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,14 @@
 <script setup>
   import MainNavbar from './components/MainNavbar.vue'
   import HeroSection from './components/HeroSection.vue'
-  import Popup from './components/Popup.vue'
-  import { ref } from 'vue'
-  import UserPopup from "./components/Popupprofile.vue"
-  import RoomSchedule from './components/RoomSchedule.vue';
 
-  const show = ref(false)
-  const success = ref(false)
-
-  const openPopup = ref(false);
-  const user = {
-    firstName: "สมพล",
-    lastName: "หยดย้อย",
-    studentId: "67022928",
-    phone: "0617584567"
-  };
-
-  function showPopup(isSuccess) {
-    success.value = isSuccess
-    show.value = true
-  }
-
-  function cancel() {
-    alert("ยกเลิกการจองเรียบร้อยแล้ว")
-    show.value = false
-  }
 </script>
 
 <template>
   <div>
     <MainNavbar />
     <HeroSection />
-  </div>
-
-  <div>
-    <button @click="showPopup(true)">จองสำเร็จ</button>
-    <button @click="showPopup(false)">จองไม่สำเร็จ</button>
-
-    <Popup 
-      :show="show"
-      :success="success"
-      @close="show=false"
-      @cancel="cancel"
-    />
-  </div>
-
-  <div>
-    <button @click="openPopup = true">ดูโปรไฟล์</button>
-
-    <UserPopup 
-      :show="openPopup" 
-      :user="user" 
-      @close="openPopup = false"
-    />
-  </div>
-  <div>
-    <RoomSchedule/>
+    <RouterView />
   </div>
 </template>
 
